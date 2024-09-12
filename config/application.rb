9099e0232801dea705ea9cb20d4dd87f131d48c9
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2024-09-11 22:19:12 -0400

    Week 13 Building an API Part 1 REST Introduction and Authentication

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module R7Rest
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -37,3 +40,4 @@ module R7Rest
     config.api_only = true
   end
 end
+
